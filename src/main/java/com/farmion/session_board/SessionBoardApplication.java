d7956981a22c7ecb45b3b874a21127eeb2051f21
younchanhyeok <ych758@naver.com>
2025-09-30 10:39:37 +0900

    Init: 개발 초기세팅

---
+++ src/main/java/com/farmion/session_board/SessionBoardApplication.java
@@ -0,0 +1,13 @@
+package com.farmion.session_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farmion/session_board/SessionBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmion.session_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
